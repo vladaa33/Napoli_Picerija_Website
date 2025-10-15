60c94b1cdd136da91f414127b3292f35acaf25e8
ailogicagency <kamikezn@gmail.com>
2025-10-15 20:38:53 +0200

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -115,7 +115,7 @@ export default function Contact() {
             </h3>
             <div className="relative w-full h-[400px] md:h-[500px] rounded-xl overflow-hidden">
               <iframe
-                src="https://www.google.com/maps?q=Napoli+Picerija,+Bulevar+Zorana+Đinđića+85,+Beograd&output=embed"
+                src="https://www.google.com/maps/embed/v1/place?q=Napoli+Picerija+Bulevar+Zorana+Djindjica+85+Beograd&zoom=15"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
