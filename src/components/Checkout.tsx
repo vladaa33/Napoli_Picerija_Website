a0d8c73d72652dbcd2fe693df9ccb16fc74673a6
vladaa33 <vladimiukic2006@gmail.com>
2025-11-12 17:58:42 +0100

    Updated Checkout.tsx

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { X, CreditCard, Banknote } from 'lucide-react';
 import { useCart } from '../context/CartContext';
 import PostalCodeSelector from './PostalCodeSelector';
